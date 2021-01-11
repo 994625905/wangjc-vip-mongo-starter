224f11c411c50cc004876d626d7a8553c06dec7a
994625905 <994625905@qq.com>
2021-01-11 14:50:59 +0800

    更新版本

--- src/main/java/vip/wangjc/mongo/annotation/MongoDB.java
+++ src/main/java/vip/wangjc/mongo/annotation/MongoDB.java
@@ -14,7 +14,7 @@ import java.lang.annotation.Target;
  */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface MongoDB {
+    public @interface MongoDB {
 
     /**
      * 名称：不可为null
